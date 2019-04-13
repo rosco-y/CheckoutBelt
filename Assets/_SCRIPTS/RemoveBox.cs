caeebc84ba9ff39cfea884edd44d354de994bcf9
Ross Ylitalo <ross.ylitalo@gmail.com>
2019-04-13 19:21:29 -0400

    Now have a prefab which generates a random price, which is displayed above the prefab with TexMeshPro TextMesh "PriceTag".  The price is also recieved by the BoxPool, and collected into an ordered list "_priceList".
    Everything is going very smoothly at this point.

--- Assets/_SCRIPTS/RemoveBox.cs
+++ Assets/_SCRIPTS/RemoveBox.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class RemoveBox : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.tag == "Player")
         {
-            MoveBox box = other.GetComponent<MoveBox>();
+            MoveBox box = other.GetComponent<MoveBox>();            
             BoxPool.Instance.ReturnToPool(box);
         }
     }
+
 
 }
